6724f4fcccba4791e5145583a095d0ad04d527b8
Coderyounes <younesanas2021@gmail.com>
2026-02-05 10:28:03 +0100

    Add watchlist store with Zustand for managing watchlist state and syncing with user session

---
+++ src/store/watchListStore.ts
@@ -0,0 +1,57 @@
+import {create} from "zustand/react";
+import {getCurrentSession, updateUser} from "../lib/storage.ts";
+import type {UserData} from "../types";
+
+interface WatchlistState {
+    watchlist: number[];
+    addToWatchlist: (movieId: number) => void;
+    removeFromWatchlist: (movieId: number) => void;
+    loadWatchlist: () => void;
+}
+
+export const useWatchlistStore = create<WatchlistState>((set, get) => ({
+    watchlist: [],
+
+    loadWatchlist: () => {
+        const user = getCurrentSession();
+        if(user && user.watchlist) {
+            const ids = user.watchlist.map(item => item.movieId);
+            set({ watchlist: ids})
+        } else {
+            set({ watchlist: []})
+        }
+    },
+    addToWatchlist: (movieId: number) => {
+        const user = getCurrentSession();
+        if(!user) return;
+
+        const currentList = get().watchlist;
+        if(currentList.includes(movieId)) return;
+
+        set({ watchlist: [...currentList, movieId]})
+
+        const updatedUser: UserData = {
+            ...user,
+            watchlist: [
+                ...user.watchlist,
+                { movieId, addedAt: new Date().toISOString()}
+            ]
+        };
+        updateUser(updatedUser);
+    },
+    removeFromWatchlist: (movieId: number) => {
+        const user = getCurrentSession();
+        if(!user) return;
+
+        set({ watchlist: get().watchlist.filter(id => id !== movieId)})
+
+        const updatedUser: UserData = {
+            ...user,
+            watchlist: user.watchlist.filter(item => item.movieId !== movieId)
+        };
+        updateUser(updatedUser);
+    },
+    isInWatchlist: (movieId: number) => {
+        return get().watchlist.includes(movieId);
+    }
+}));
